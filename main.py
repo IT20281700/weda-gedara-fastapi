d7ee2df1b0789f1c742cc64cfd4ea46454b9cd4b
Chamod Ishankha <it20281700@my.sliit.lk>
2023-06-09 15:25:28 +0530

    fastapi with env

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime, timedelta
